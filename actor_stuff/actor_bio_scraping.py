de19e1827019d7fbc433ae94c4bf396c385ec131
Arjun Gupta <arjungup740@gmail.com>
2022-02-27 16:46:04 -0500

    counts largely working

--- actor_stuff/actor_bio_scraping.py
+++ actor_stuff/actor_bio_scraping.py
@@ -43,28 +43,31 @@ list_of_1000_actors['wikipedia_url'] = 'https://en.wikipedia.org/wiki/' + list_o
 # end = datetime.datetime.now()
 # print(end - start)
 
-target_words_list = ['alcohol', 'alcoholism', 'rehab', 'drugs', 'drug' 'abuse', 'addiction', 'addicted', 'sober since', 'drug offense', 'overdose', 'overdosing', 'cocaine', 'drug use']
-actor_name = 'Aaron_Eckhart'
+target_words_list = ['alcohol', 'alcoholism', 'rehab', 'drugs', 'drug abuse', 'addiction', 'addicted', 'sober since', 'drug offense', 'overdose', 'overdosing', 'cocaine', 'sober', 'drug use']
+# actor_name = 'Ben_Affleck'
 # pd.read_txt(f'{os.getcwd()}/actor_stuff/actor_bios/Aaron_Eckhart')
-
-
 ## check counts of words
 
 big_dict = {}
 for actor_name in sorted(os.listdir(f'{os.getcwd()}/actor_stuff/actor_bios/')):
 	with open(f'{os.getcwd()}/actor_stuff/actor_bios/{actor_name}') as f:
-		lines = f.readlines()
+		lines = f.read()
 		key_words_dict = {}
 		for word in target_words_list:
-			key_words_dict[word] = Counter(lines[0].split(' '))[word]
+			key_words_dict[word] = Counter(lines.split(' '))[word]
 		big_dict[actor_name] = key_words_dict
 
 raw = pd.DataFrame(big_dict).reset_index().rename(columns = {'index':'word'})
 raw.head()
 df = pd.pivot_table(pd.melt(raw, id_vars = ['word'], var_name = 'actor', value_name = 'value'), index = 'actor', columns = 'word', values = 'value').reset_index()
+df[df['actor'] == 'Angela_Lansbury']
 
-df.sum()
-df[df['actor'] == 'Brad_Pitt']
+scores = df.set_index('actor').sum(axis = 1)
+scores[scores > 5]
+
+
 # todo AG: grab all the h1, 2s, 3s, if any of them have the term then they definitely have had it
 
 # todo AG: can try training some sort of ML the way the name thing did
+
+# can't do n-grams, just single words
